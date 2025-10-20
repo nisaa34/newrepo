6ef64df1358cd374a9908399198f6a9939ae87cb
Hayrunnisa topuz <hayrunnisa.topuz@ogr.sakarya.edu.tr>
2025-10-20 21:32:44 +0300

    yeniden güncellendi

--- ders.cpp
+++ ders.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "I'm learning github" << endl;
+    cout << "bence yapabiliyorum"<< endl ;
     cout << " ben nisa" << endl;
  return 0;
 }
